fa29bbab06c56a31d3065cc2b3622e5ded8062a9
Joe Hawes <morehawes@gmail.com>
2022-05-31 20:35:43 -0700

    Use correct header in plugin description. (#8)

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@
 /**
  * Plugin Name: Plugin Dependencies
  * Plugin URI:  https://github.com/WordPress/wp-plugin-dependencies
- * Description: Parses 'Requires Plugin' header, add plugin install dependencies tab, and information about dependencies.
+ * Description: Parses 'Requires Plugins' header, add plugin install dependencies tab, and information about dependencies.
  * Author: Andy Fragen, Colin Stewart
  * Version: 0.16.2
  * License: MIT
