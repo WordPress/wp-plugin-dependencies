fcfa40587b25e85c99c2c911c1b815f9b8c7cdbf
Andy Fragen <andy@thefragens.com>
2022-10-19 23:29:18 -0700

    fix my errant suggestion of Colin's PR , bad Andy

--- wp-admin/includes/class-wp-plugin-dependencies.php
+++ wp-admin/includes/class-wp-plugin-dependencies.php
@@ -420,11 +420,13 @@ class WP_Plugin_Dependencies {
 					continue;
 				}
 
+				$name_attr              = esc_attr( $name );
 				$details_links[ $name ] = sprintf(
 					'<a href="%s" class="thickbox open-plugin-details-modal" aria-label="%s" data-title="%s">%s</a>',
 					esc_url( network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=' . $plugin_data['slug'] . '&TB_iframe=true&width=600&height=550' ) ),
 					/* translators: %s: Plugin name. */
-					sprintf( __( 'More information about %s' ), esc_attr( $name ) ),
+					sprintf( __( 'More information about %s' ), $name_attr ),
+					$name_attr,
 					$name
 				);
 			}
