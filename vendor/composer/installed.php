f4443fa5f79d94f1c04332dabacdadfa8dfb65e2
Andy Fragen <andy@thefragens.com>
2022-10-22 18:06:10 -0700

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wordpress/wp-plugin-dependencies',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '57da233d6f844fc72f45881d4a768dad69b1ec8b',
+        'reference' => 'e430064ebe8063ef21aff5ba54a92514c1784552',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'afragen/add-plugin-dependency-api' => array(
-            'pretty_version' => '0.4.3',
-            'version' => '0.4.3.0',
-            'reference' => '1fba498ee5e3a93df713aa1b5bb1d568507450ef',
+            'pretty_version' => '0.4.4',
+            'version' => '0.4.4.0',
+            'reference' => '6bdf298be6866078760044b6b433622562f7afe1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../afragen/add-plugin-dependency-api',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'wordpress/wp-plugin-dependencies' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '57da233d6f844fc72f45881d4a768dad69b1ec8b',
+            'reference' => 'e430064ebe8063ef21aff5ba54a92514c1784552',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
