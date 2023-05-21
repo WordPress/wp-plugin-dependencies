d7a64ba117167cd08d95f4516c4426a207f61101
costdev <me@colin-stewart.co.uk>
2023-05-21 22:37:12 +0100

    Update docblock with shape of `$data`.

--- wp-admin/includes/plugin-install.php
+++ wp-admin/includes/plugin-install.php
@@ -14,9 +14,15 @@
  * Gets the markup for the plugin install action button.
  *
  * @param string       $name           Plugin name.
- * @param array|object $data           Plugin data.
- * @param bool         $compatible_php PHP compatibility check.
- * @param bool         $compatible_wp  WP compatibilty check.
+ * @param array|object $data           {
+ *     An array or object of plugin data. Can be retrieved from the API.
+ *
+ *     @type string   $slug             The plugin slug.
+ *     @type string[] $requires_plugins An array of plugin dependency slugs.
+ *     @type string   $version          The plugin's version string. Used when getting the install status.
+ * }
+ * @param bool         $compatible_php The result of a PHP compatibility check.
+ * @param bool         $compatible_wp  The result of a WP compatibilty check.
  *
  * @return string $button The markup for the dependency row button.
  */
