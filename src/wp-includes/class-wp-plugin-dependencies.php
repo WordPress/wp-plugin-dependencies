1e05586963c13dc5ddf26123003f070847b86e5c
Andy Fragen <andy@thefragens.com>
2023-10-03 13:44:01 -0700

    note to remove for PR

--- src/wp-includes/class-wp-plugin-dependencies.php
+++ src/wp-includes/class-wp-plugin-dependencies.php
@@ -527,7 +527,8 @@ class WP_Plugin_Dependencies {
 				continue;
 			}
 
-			self::$plugins[ $plugin ]['RequiresPlugins'] = $header['RequiresPlugins'];
+			self::$plugins[ $plugin ]['RequiresPlugins'] = $header['RequiresPlugins']; // TODO: remove for PR.
+
 			$dependency_slugs                            = self::sanitize_dependency_slugs( $header['RequiresPlugins'] );
 			self::$dependencies[ $plugin ]               = $dependency_slugs;
 			self::$dependency_slugs                      = array_merge( self::$dependency_slugs, $dependency_slugs );
